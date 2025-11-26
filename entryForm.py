280fbfb8d78065b67f2c59ff0da21862c9ca68a5
toqa-als <154462776+toqa-als@users.noreply.github.com>
2025-11-25 22:55:14 -0500

    Update entryForm.py

--- entryForm.py
+++ entryForm.py
@@ -1,26 +1,48 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import font
 
 class EntryForm:
     def __init__(self, parent, bg="#E9F6FB"):
         self.parent = parent
         self.bg = bg
+
+        #named fonts to be used so it doesnt look different on diifferent devices like OS , mac
+        self.labelFont = font.Font(family="Arial", size=12, weight="bold")
+        self.entryFont = font.Font(family="Arial", size=11)
+
+        #ttk style for the Combobox
+        style = ttk.Style()
+        style.configure("My.TCombobox", font=self.entryFont)
+
         self.build()
 
     def build(self):
-        #food label
-        tk.Label(self.parent, text="Food Name:", bg=self.bg, font=("Arial", 12, "bold")).grid(row=0, column=1, sticky="w", padx=10)
-        self.foodEntry = tk.Entry(self.parent, width=28, font=("Arial", 11))
+        #food labels
+        tk.Label(self.parent, text="Food Name:", bg=self.bg, font=self.labelFont)\
+            .grid(row=0, column=1, sticky="w", padx=10)
+
+        self.foodEntry = tk.Entry(self.parent, width=28, font=self.entryFont)
         self.foodEntry.grid(row=0, column=2, columnspan=2, sticky="w", padx=5)
 
-        #calories labell
-        tk.Label(self.parent, text="Calories:", bg=self.bg, font=("Arial", 12, "bold")).grid(row=0, column=4, sticky="w", padx=10)
-        self.calEntry = tk.Entry(self.parent, width=10, font=("Arial", 11))
+        #calories labels
+        tk.Label(self.parent, text="Calories:", bg=self.bg, font=self.labelFont)\
+            .grid(row=0, column=4, sticky="w", padx=10)
+
+        self.calEntry = tk.Entry(self.parent, width=10, font=self.entryFont)
         self.calEntry.grid(row=0, column=5, sticky="w", padx=5)
 
-        #the meal type
-        tk.Label(self.parent, text="Meal type:", bg=self.bg, font=("Arial", 12, "bold")).grid(row=1, column=2, sticky="e", padx=10)
-        self.mealCombo = ttk.Combobox(self.parent, values=["Breakfast", "Lunch", "Dinner", "Snack"], width=18, state="readonly")
+        #the meal types
+        tk.Label(self.parent, text="Meal type:", bg=self.bg, font=self.labelFont)\
+            .grid(row=1, column=2, sticky="e", padx=10)
+
+        self.mealCombo = ttk.Combobox(
+            self.parent,
+            values=["Breakfast", "Lunch", "Dinner", "Snack"],
+            width=18,
+            state="readonly",
+            style="My.TCombobox"     # apply custom font
+        )
         self.mealCombo.grid(row=1, column=3, sticky="w", padx=5)
         self.mealCombo.set("Breakfast")
 
@@ -28,5 +50,3 @@ class EntryForm:
         self.foodEntry.delete(0, "end")
         self.calEntry.delete(0, "end")
         self.mealCombo.set("")
-
-        
